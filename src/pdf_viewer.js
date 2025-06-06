2ffed8c3dcf506a5893f783c045af95b4126f54a
Christopher M <christopher_mao@branson.org>
2025-06-06 09:19:42 -0700

    Update Loading Animation

--- src/pdf_viewer.js
+++ src/pdf_viewer.js
@@ -44,38 +44,8 @@ export default function PDFLoader({ file = "/Spring2024.pdf" }) {
                     ref={setContainerRef}
                 >
                     {loading && (
-                        <main className="loading-container">
-                            <svg className="loading-ip" viewBox="0 0 256 128" width="256px" height="128px" xmlns="http://www.w3.org/2000/svg">
-                                <defs>
-                                    <linearGradient id="grad1" x1="0" y1="0" x2="1" y2="0">
-                                        <stop offset="0%" stopColor="#000000" />
-                                        {/* <stop offset="33%" stopColor="#ffb900" />
-                                        <stop offset="67%" stopColor="#f78200" /> */}
-                                        <stop offset="100%" stopColor="#000000" />
-                                    </linearGradient>
-                                    <linearGradient id="grad2" x1="1" y1="0" x2="0" y2="0">
-                                        <stop offset="0%" stopColor="#000000" />
-                                        {/* <stop offset="33%" stopColor="#973999" />
-                                        <stop offset="67%" stopColor="#009cdf" /> */}
-                                        <stop offset="100%" stopColor="#000000" />
-                                    </linearGradient>
-                                    {/* 3452eb, 0095ff */}
-                                </defs>
-                                <g fill="none" strokeLinecap="round" strokeWidth="16">
-                                    <g className="loading-ip__track" stroke="#ddd">
-                                        <path d="M8,64s0-56,60-56,60,112,120,112,60-56,60-56" />
-                                        <path d="M248,64s0-56-60-56-60,112-120,112S8,64,8,64" />
-                                    </g>
-                                    <g strokeDasharray="180 656">
-                                        <path className="loading-ip__worm1" stroke="url(#grad1)" strokeDashoffset="0" d="M8,64s0-56,60-56,60,112,120,112,60-56,60-56" />
-                                        <path className="loading-ip__worm2" stroke="url(#grad2)" strokeDashoffset="358" d="M248,64s0-56-60-56-60,112-120,112S8,64,8,64" />
-                                    </g>
-                                </g>
-                            </svg>
-                        </main>
+                        <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
                     )}
-
-
 
                     <Document
                         file={file}
@@ -93,6 +63,7 @@ export default function PDFLoader({ file = "/Spring2024.pdf" }) {
                                         ? Math.min(containerWidth, maxWidth)
                                         : maxWidth
                                 }
+                                style="margin: 0;"
                             />
                         ))}
                     </Document>
