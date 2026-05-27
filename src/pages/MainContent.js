4c7b20ab39c88fd1345d63ddc287aff03eb22d09
DaBestForSure <125160337+DaBestForSure@users.noreply.github.com>
2026-05-27 08:51:46 -0700

    typo

--- src/pages/MainContent.js
+++ src/pages/MainContent.js
@@ -5,7 +5,7 @@ import BackToTop from '../components/BackToTop.jsx'; // Ensure to import your Ba
 const MainContent = () => {
   return (
     <div style={{ padding: '20px', marginTop: '10px'}}>
-      <PDFLoader file='/Spring2026.pdf'/>
+      <PDFLoader file='/spring2026.pdf'/>
       <BackToTop />
     </div>
   );
